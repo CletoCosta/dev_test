166d55d9b1028653cd39d4ca8b45e446de173636
CletoCosta <silvacletto@gmail.com>
2024-11-17 10:43:01 -0300

    Update User.ts
    Signed-off-by: CletoCosta <silvacletto@gmail.com>

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-
 //TODO Crie a entidade de User
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm';
+import { Post } from './Post';  // Importando a entidade Post
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  email: string;
+
+  // Relacionamento "um para muitos" com a tabela Post
+  @OneToMany(() => Post, (post) => post.user)
+  posts: Post[];
+}
